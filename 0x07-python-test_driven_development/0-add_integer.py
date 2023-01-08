b6c500ad5158cbb17af8c425577ff44fbc8463fa
avndon <donclark4live@gmail.com>
2023-01-08 06:14:22 -0800

    edited some files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,7 +13,6 @@ def add_integer(a, b=98):
         The addition of the two given numbers
     Raises:
         TypeError: If a or b aren't integer and/or float numbers
-
     """
 
     if not isinstance(a, int) and not isinstance(a, float):
